6d730ea1e2bcf566e8b14257d9cd2e32763eee22
Khoa <trandangkhoa010205@gmail.com>
2026-02-10 17:42:06 +0700

    Reset project - Spring Boot Glasses Store

---
+++ src/main/java/com/fpt/dangkhoa/GlassesStoreApplication.java
@@ -0,0 +1,12 @@
+package com.fpt.dangkhoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassesStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlassesStoreApplication.class, args);
+    }
+}
